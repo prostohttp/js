2db084ecacb5865b44d583114e2d76061d8632fc
prostohttp <sellmar38@gmail.com>
2018-08-06 00:08:36 +1000

    test 777

--- js/script.js
+++ js/script.js
@@ -1,10 +1,2 @@
-// let fs = require('fs');
-// fs.writeFileSync('./test.txt', 'hello world!!!!');
-// fs.writeFileSync('./test1.txt', 'hello world 2!!!!');
 
-let user = {
-	'name': 'Mikele',
-	'lastName': 'Corleone'
-}
 
-console.log(`${user.name} ${user.lastName}`);
