c24a72df2b8893bc9710a2e6f63252a7246c47e7
prostohttp <sellmar38@gmail.com>
2018-08-06 21:55:29 +1000

    test webstorm 2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
-const fs = require('fs');
 let name = 'Mikele';
 let lastName = 'Corleone';
 console.log(`${name} ${lastName}`);
+
+const arr = [
+    {
+        name: 'Mikele',
+        lastName: 'Corleone'
+    }
+];
