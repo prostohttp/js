260830cc77a9697fadf807ba4ef0a8631193fa5c
prostohttp <sellmar38@gmail.com>
2018-08-22 21:10:14 +1000

    Проверяем работу классов, их наследование и т.д.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,30 @@
-function Point(x, y) {
-	this.x = x;
-	this.y = y;
-	console.log(`${x % y}`);
+// function Point(x, y) {
+// 	this.x = x;
+// 	this.y = y;
+// 	console.log(`${x % y}`);
+// }
+
+// let p = new Point(100, 20);
+// let d = new Point(200, 20);
+
+class Human {
+    constructor() {
+        console.log(`PARENT`);
+    }
 }
 
-let p = new Point(100, 20);
-let d = new Point(200, 20);
+class FullName extends Human {
+    constructor(name, lastName) {
+        super();
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    sayHi() {
+        console.log(`${this.name} ${this.lastName}`);
+    }
+}
+
+const name = new FullName('Mikele', 'Corleone');
+
+console.log(name.sayHi());
